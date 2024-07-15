244e042a125359e31a824c0c3bc8dbccffb6f9fe
Yusuf Berkay Usluer <yusufberkayusluer@gmail.com>
2024-07-15 11:09:42 +0300

    +admin +admincontrollerViewModels+Login+Register

---
+++ KutuphaneUygulamasi/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KutuphaneUygulamasi.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+
+        public IActionResult ListBooks()
+        {
+            return View();
+        }
+
+        public IActionResult ListMembers()
+        {
+            return View();
+        }
+
+        public IActionResult AddMember()
+        {
+            return View();
+        }
+
+        public IActionResult SetMemberRole()
+        {
+            return View();
+        }
+    }
+}
