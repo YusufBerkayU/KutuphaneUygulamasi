f6ec8ae81837ff92536bd4f0f8e3608ff58c3726
Yusuf Berkay Usluer <yusufberkayusluer@gmail.com>
2024-07-16 13:58:08 +0300

    +members + books

---
+++ KutuphaneUygulamasi/Models/Member.cs
@@ -0,0 +1,19 @@
+﻿namespace KutuphaneUygulamasi.Models
+{
+    public class Member
+    {
+        
+       
+        public string Email { get; set; }
+
+        public string Firstname { get; set; }   
+
+        public string Lastname { get; set; }    
+
+        public string Address { get; set; }   
+        
+        public string Password { get; set; }
+
+
+    }
+}
