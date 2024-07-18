c20778e283bccb72d00410e9c0cfd41ae41bc68e
Yusuf Berkay Usluer <yusufberkayusluer@gmail.com>
2024-07-18 10:06:28 +0300

    AddMember+Layout

--- KutuphaneUygulamasi/Controllers/AdminController.cs
+++ KutuphaneUygulamasi/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using KutuphaneUygulamasi.Data;
 using KutuphaneUygulamasi.Models;
+using KutuphaneUygulamasi.Models.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -41,9 +42,40 @@ namespace KutuphaneUygulamasi.Controllers
             return View(users);
         }
 
+        [HttpGet]
         public IActionResult AddMember()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMember(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Address = model.Address
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListMembers");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
         }
 
         public IActionResult SetMemberRole()
